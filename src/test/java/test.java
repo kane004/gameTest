a7fa3a6ab9ac81ad7d022ed32e8a55f7d26be300
kane <kanetest811@gmail.com>
2023-10-15 11:20:41 +0400

    更新驱动

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -17,7 +17,7 @@ public class test {
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "D:\\GoogleChrome_79.0.3945.130\\ChromePortable\\App\\Google Chrome\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://uat.marke88.com/cms/?#/login");
         driver.manage().window().maximize();
