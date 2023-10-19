911cf30d6f3dc7966c26dc9b482eca11e49c4f02
kane004 <kanetest811@gmail.com>
2023-10-19 18:33:39 +0400

    新增用例

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -50,16 +50,32 @@ public class test {
     @Test(priority = 1)
     public void validLoginTest() throws InterruptedException {
         pageActions.sendKeyID("800");
-        Thread.sleep(1000);
         pageActions.enterUsername("kane01");
-        Thread.sleep(1000);
         pageActions.enterPassword("kane01");
-        Thread.sleep(1000);
         pageActions.clickLoginButton();
         Assertion.verifyEquals(1,1);
         Assert.assertTrue(Assertion.currentFlag());
 
     }
+
+
+    @Features("财务管理")
+    @Stories("会员对账")
+    @Title("验证会员对账差别金额数据第一条数据")
+    @Severity(SeverityLevel.BLOCKER)
+    @Step("1.进入会员对账，2.计算对账差别金额")
+    @Description("验证会员对账差别金额第一条数据")
+    @Test(priority = 2)
+    public void test2() throws InterruptedException {
+       
+        Assertion.verifyEquals(1,2);
+        Assert.assertTrue(Assertion.currentFlag());
+
+    }
+
+
+
+
 
     @AfterClass
     public void tearDown() {
